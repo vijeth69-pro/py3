04c7d79bc94e9a7df62b708619f4350af2550d43
vijeth69-pro <captainmncaptain@gmail.com>
2025-03-08 16:57:17 +0530

    Initial commit

---
+++ caldemo2.java
@@ -0,0 +1,30 @@
+//calculator srp
+class calculator
+{
+public static int add(int x,int y)
+{return x+y;}
+public static int sub(int x,int y)
+{return x-y;}
+public static int mul(int x,int y)
+{return x*y;}
+public static int div(int x,int y)
+{return x/y;}
+}
+class Resultprinter
+{public static void display(int value)
+{System.out.println("The value is:"+value);
+}
+}
+class caldemo2
+{public static void main(String args[])
+{int a=calculator.add(20,30);
+Resultprinter.display(a);
+int b=calculator.sub(20,30);
+Resultprinter.display(b);
+int c=calculator.mul(20,30);
+Resultprinter.display(c);
+int d=calculator.div(20,30);
+Resultprinter.display(d);
+}
+}
+
